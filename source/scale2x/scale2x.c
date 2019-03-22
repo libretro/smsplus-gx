7b307cce26c798c15478d2a9daa0836399c72bac
gameblabla <gameblabla@openmailbox.org>
2019-03-22 08:38:02 +0100

    Change the Scale2 code a bit as to make use of 16-bits pointers instead of 8-bits pointers. This should greatly improve the speed on devices like the RS-97 and GCW0.

--- source/scale2x/scale2x.c
+++ source/scale2x/scale2x.c
@@ -1,66 +1,64 @@
-/*
-    pygame - Python Game Library
-    Copyright (C) 2000-2001  Pete Shinners
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-    MA  02110-1301  USA
-    Pete Shinners
-    pete@shinners.org
-*/
+/* scaler.c: code for selecting (etc) scalers
+ * Copyright (C) 2003 Fredrick Meunier, Philip Kendall
+ * Copyright (c) 2015 Sergio Baldoví
+ * 
+ * $Id: scaler.c 5432 2016-05-01 04:16:09Z fredm $
+ *
+ * Originally taken from ScummVM - Scumm Interpreter
+ * Copyright (C) 2001  Ludvig Strigeus
+ * Copyright (C) 2001/2002 The ScummVM project
+ *
+ * HQ2x and HQ3x scalers taken from HiEnd3D Demos (http://www.hiend3d.com)
+ * Copyright (C) 2003 MaxSt ( maxst@hiend3d.com )
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
 
-/*
-   This implements the AdvanceMAME Scale2x feature found on this page,
-   http://advancemame.sourceforge.net/scale2x.html
-   It is an incredibly simple and powerful image doubling routine that does
-   an astonishing job of doubling game graphic data while interpolating out
-   the jaggies. Congrats to the AdvanceMAME team, I'm very impressed and
-   surprised with this code!
-*/
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
 
-/* https://github.com/CTPUG/pygame_cffi/blob/master/cffi_builders/lib/scale2x.c */
-#include <SDL/SDL.h>
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
 #include "scale2x.h"
 
-#define MAX(a,b)    (((a) > (b)) ? (a) : (b))
-#define MIN(a,b)    (((a) < (b)) ? (a) : (b))
+/* No license ? Scale2x was licensed under the GPLv2 and this code was found among in GPLv2 licensed code.
+ * I had to make some minor modifications to it and the code is fairly small but i think we should be good. */
 
-/* This requires a destination surface already setup to be twice as large as the source.
- * Only for 16-bits */
-
-void scale2x(uint16_t* restrict srcpixels, uint16_t* restrict dstpixels, const int32_t srcpitch, const int32_t dstpitch, const int32_t width, const int32_t height)
+void scale2x(uint16_t* restrict srcpixels, uint16_t* restrict dstpixels, const uint32_t srcpitch, const uint32_t dstpitch, const uint32_t width, uint32_t height)
 {
-    int32_t looph, loopw;
-    uint8_t* restrict srcpix = (uint8_t* restrict)srcpixels;
-    uint8_t* restrict dstpix = (uint8_t* restrict)dstpixels;
-	uint16_t E0, E1, E2, E3, B, D, E, F, H;
-	for(looph = 0; looph < height; ++looph)
+	uint32_t nextlineSrc = srcpitch / sizeof(uint16_t);
+	uint16_t* restrict p = (uint16_t* restrict)srcpixels;
+
+	uint32_t nextlineDst = dstpitch / sizeof(uint16_t);
+	uint16_t* restrict q = (uint16_t* restrict)dstpixels;
+	
+	uint32_t i, j;
+	uint16_t B, D, E, F, H;
+	
+	while(height--) 
 	{
-		for(loopw = 0; loopw < width; ++loopw)
-		{
-			B = *(uint16_t* restrict)(srcpix + (MAX(0,looph-1)*srcpitch) + (2*loopw));
-			D = *(uint16_t* restrict)(srcpix + (looph*srcpitch) + (2*MAX(0,loopw-1)));
-			E = *(uint16_t* restrict)(srcpix + (looph*srcpitch) + (2*loopw));
-			F = *(uint16_t* restrict)(srcpix + (looph*srcpitch) + (2*MIN(width-1,loopw+1)));
-			H = *(uint16_t* restrict)(srcpix + (MIN(height-1,looph+1)*srcpitch) + (2*loopw));
+		j = 0;
+		for(i = 0; i < width; ++i, j += 2) {
+			B = *(p + i - nextlineSrc);
+			D = *(p + i - 1);
+			E = *(p + i);
+			F = *(p + i + 1);
+			H = *(p + i + nextlineSrc);
 
-			E0 = D == B && B != F && D != H ? D : E;
-			E1 = B == F && B != D && F != H ? F : E;
-			E2 = D == H && D != B && H != F ? D : E;
-			E3 = H == F && D != H && B != F ? F : E;
-
-			*(uint16_t* restrict)(dstpix + looph*2*dstpitch + loopw*2*2) = E0;
-			*(uint16_t* restrict)(dstpix + looph*2*dstpitch + (loopw*2+1)*2) = E1;
-			*(uint16_t* restrict)(dstpix + (looph*2+1)*dstpitch + loopw*2*2) = E2;
-			*(uint16_t* restrict)(dstpix + (looph*2+1)*dstpitch + (loopw*2+1)*2) = E3;
+			*(q + j) = (uint16_t)(D == B && B != F && D != H ? D : E);
+			*(q + j + 1) = (uint16_t)(B == F && B != D && F != H ? F : E);
+			*(q + j + nextlineDst) = (uint16_t)(D == H && D != B && H != F ? D : E);
+			*(q + j + nextlineDst + 1) = (uint16_t)(H == F && D != H && B != F ? F : E);
 		}
+		p += nextlineSrc;
+		q += nextlineDst << 1;
 	}
+
 }
--- source/scale2x/scale2x.h
+++ source/scale2x/scale2x.h
@@ -30,6 +30,6 @@
 
 #ifndef SCALE2X_H
 #define SCALE2X_H
-void scale2x(uint16_t* restrict srcpixels, uint16_t* restrict dstpixels, const int32_t srcpitch, const int32_t dstpitch, const int32_t width, const int32_t height);
+void scale2x(uint16_t* restrict srcpixels, uint16_t* restrict dstpixels, const uint32_t srcpitch, const uint32_t dstpitch, const uint32_t width, uint32_t height);
 #endif
 
