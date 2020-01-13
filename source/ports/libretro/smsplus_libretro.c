e15dded52cd2518c1a7ce219284869f9086f91f9
negativeExponent <negativeExponent@users.noreply.github.com>
2020-01-13 13:54:20 +0800

    Add support for input bitmasks

--- source/ports/libretro/smsplus_libretro.c
+++ source/ports/libretro/smsplus_libretro.c
@@ -144,6 +144,10 @@ static uint32_t sdl_controls_update_input(int k, int32_t p)
 {
    (void)k;
    (void)p;
+<<<<<<< HEAD
+=======
+   return 0;
+>>>>>>> Add support for input bitmasks
 }
 
 static void bios_init()
