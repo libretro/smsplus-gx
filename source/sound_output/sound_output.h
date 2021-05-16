686b818869e9f6b494ca4af6150f1b946dffc641
negativeExponent <54053706+negativeExponent@users.noreply.github.com>
2021-05-17 06:12:48 +0800

    Change C++ comments to C comments

--- source/sound_output/sound_output.h
+++ source/sound_output/sound_output.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 extern void Sound_Init(void);
-//extern void Sound_Update(void);
+/* extern void Sound_Update(void); */
 extern void Sound_Update(int16_t* sound_buffer, unsigned long len);
 extern void Sound_Close(void);
 extern void Sound_Pause(void);
